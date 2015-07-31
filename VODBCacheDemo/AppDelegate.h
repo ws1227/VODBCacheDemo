2b063d3d8ead194b094ae18b697ed805a7e66ab0
pozi119 <pozi119@163.com>
2015-07-31 14:11:52 +0800

    Init commit

---
+++ VODBCacheDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VODBCacheDemo
+//
+//  Created by Valo on 15/7/31.
+//  Copyright (c) 2015年 Valo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VODBCacheDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VODBCacheDemo
+//
+//  Created by Valo on 15/7/31.
+//  Copyright (c) 2015年 Valo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
